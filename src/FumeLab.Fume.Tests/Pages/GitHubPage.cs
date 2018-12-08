b9a3df35388cb3947655cbca5e03f43b7ffa5fa1
Filip Pausic <f.pausic@levi9.com>
2018-12-08 07:03:50 +0100

    added test pages

---
+++ src/FumeLab.Fume.Tests/Pages/GitHubPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FumeLab.Fume.Core;
+using FumeLab.Fume.Core.Elements;
+using FumeLab.Fume.Core.Selectors;
+
+namespace FumeLab.Fume.Tests.Pages
+{
+    public class GitHubPage
+    {
+        //elements
+        [Selector(Type = typeof(XPath), Value = "//input[@placeholder='Search GitHub']")]
+        public Input SearchGitHub { get; set; }
+
+        [Selector(Type = typeof(XPath), Value = "//a[@href='/FumeLab/fumelab.github.io']")]
+        public Clickable FumeLabLink { get; set; }
+
+        [Selector(Type = typeof(XPath), Value = "//div[contains(text(),'FumeLab')]")]
+        public Clickable JumpToFume { get; set; }
+
+        [Selector(Type = typeof(XPath), Value = "//a[@href='/FumeLab']")]
+        public Clickable FumeLabBaseLink { get; set; }
+
+        [Selector(Type = typeof(XPath), Value = "//a[@title='https://fumelab.github.io']")]
+        public Clickable FumeSiteLink { get; set; }
+    }
+}
